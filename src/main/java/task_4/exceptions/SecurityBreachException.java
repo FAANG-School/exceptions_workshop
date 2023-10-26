9b8d5bba1b7726cd7dce1c090abc4ee633662c4f
kislyakoff <kislyakoff@tut.by>
2023-10-26 17:55:21 +0300

    add task4 solution

---
+++ src/main/java/task_4/exceptions/SecurityBreachException.java
@@ -0,0 +1,9 @@
+package task_4.exceptions;
+
+public class SecurityBreachException extends RuntimeException {
+
+	public SecurityBreachException(String message) {
+		super(message);
+	}
+
+}
---
+++ src/main/java/task_4/exceptions/UnauthorizedAccessAttemptException.java
@@ -0,0 +1,9 @@
+package task_4.exceptions;
+
+public class UnauthorizedAccessAttemptException extends RuntimeException {
+
+	public UnauthorizedAccessAttemptException(String message) {
+		super(message);
+	}
+
+}
